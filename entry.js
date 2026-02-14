652cb8789bb8590c1eb9ef246381aafd01bd5e89
rantyclarke-coder <rantyclarke@gmail.com>
2026-02-14 15:08:59 +0530

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,1 +1,18 @@
-entry
+const menuBtn = document.querySelector(".menu");
+const sideMenu = document.getElementById("sideMenu");
+const overlay = document.getElementById("overlay");
+const closeMenu = document.getElementById("closeMenu");
+
+function openMenu() {
+  sideMenu.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function hideMenu() {
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+menuBtn.addEventListener("click", openMenu);
+overlay.addEventListener("click", hideMenu);
+closeMenu.addEventListener("click", hideMenu);
