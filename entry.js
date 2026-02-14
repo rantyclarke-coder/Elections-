ebe1db13dcee2a2c7f46b2e9633b13c6d80ad1aa
rantyclarke-coder <rantyclarke@gmail.com>
2026-02-14 16:32:10 +0530

    Update entry.js

--- entry.js
+++ entry.js
@@ -20,14 +20,29 @@ closeMenu.addEventListener("click", hideMenu);
 const themeToggle = document.getElementById("themeToggle");
 const logo = document.getElementById("logo");
 
+/* APPLY SAVED THEME ON LOAD */
+
+if(localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  logo.src = "images/wpdark.png";
+}
+
+/* TOGGLE THEME */
+
 themeToggle.addEventListener("click", function() {
 
   document.body.classList.toggle("dark");
 
   if(document.body.classList.contains("dark")) {
+
     logo.src = "images/wpdark.png";
+    localStorage.setItem("theme", "dark");
+
   } else {
+
     logo.src = "images/wplight.png";
+    localStorage.setItem("theme", "light");
+
   }
 
 });
