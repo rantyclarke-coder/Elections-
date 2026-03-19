6d9fe54bb67e5f682c5710262cfb2b4c0bedf483
rantyclarke-coder <rantyclarke@gmail.com>
2026-03-20 00:13:55 +0530

    Update Google Sheet URL in house.js

--- house.js
+++ house.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 // ================== CONFIG ==================
 
 // 🔗 Google Sheet CSV
-const SHEET_URL = "PASTE_YOUR_WORKING_LINK";
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSsbbXqdgfMGosYWjOVNR-2UUw6bZzjGNtnfuuWpbBuTutk6Jm1lffgHUis8GNjfQLFZLkaSpJNlck2/pubhtml?gid=616046635&single=true";
 
 // 🎨 PARTY COLORS
 const PARTIES = {
